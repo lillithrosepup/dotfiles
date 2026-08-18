5b64103a44feaf36e17371befb8d2f4d5053ed0c
Lillith Rose <lillith@abyssalfragments.org>
2026-08-18 09:30:37 -0400

    window rules

---
+++ .config/hypr/lillith/windowrules.lua
@@ -0,0 +1,10 @@
+hl.window_rule(
+  {
+    name = "wayvr-uidev-float",
+    match = {initial_title = "\\[-\\/=\\]: gui scale, F10: debug draw, F11: print tree"},
+    float = true,
+    no_focus = true,
+    size = {450, 300},
+    center = true
+  }
+)
